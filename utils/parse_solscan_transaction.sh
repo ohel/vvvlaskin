8432bb8ccec92d5c48339f4d1d22de22841c328d
ohel <ohel@users.noreply.github.com>
2025-02-17 03:59:23 +0200

    ETH transaction improvements.

--- utils/parse_solscan_transaction.sh
+++ utils/parse_solscan_transaction.sh
@@ -25,7 +25,7 @@ read main_currency_price
 [ "$(echo $main_currency_price | grep -o ,)" ] && main_currency_price=$(echo $main_currency_price | cut -f 2 -d ',')
 
 default_exchange="Raydium"
-[ "$main_currency" = "ETH" ] && default_exchange="Uniswap V2"
+[ "$main_currency" = "ETH" ] && default_exchange="Uniswap"
 echo -n "Enter exchange [$default_exchange]: "
 read exchange
 [ ! "$exchange" ] && exchange="$default_exchange"
@@ -38,12 +38,21 @@ read currency_bought
 [ ! "$currency_bought" ] && currency_bought="$main_currency"
 [ "$currency_sold" = "$currency_bought" ] && echo "Check currencies." && exit 1
 
-echo -n "Enter amount of $currency_sold sold/swapped from (token balance change including fees): "
+fee_text="including" && [ "$main_currency" = "ETH" ] && fee_text="excluding"
+echo -n "Enter amount of $currency_sold sold/swapped from (token balance change $fee_text fees): "
 read s_amount
 s_amount=$(echo $s_amount | tr -d -c '[0-9].')
-echo -n "Enter amount of $currency_bought bought/swapped to (token balance change including fees): "
+echo -n "Enter amount of $currency_bought bought/swapped to (token balance change $fee_text fees): "
 read b_amount
 b_amount=$(echo $b_amount | tr -d -c '[0-9].')
+
+if [ "$main_currency" = "ETH" ]
+then
+    echo -n "Enter transaction fee in ETH: "
+    read vc_fee
+    vc_fee=$(echo $vc_fee | tr -d -c '[0-9].')
+    vc_text="\"vcfee\": $vc_fee, "
+fi
 
 comment="Realize value for $currency_sold->$currency_bought swap. Value based on $main_currency price: $main_currency_price"
 
@@ -51,20 +60,19 @@ if [ "$currency_sold" = "$main_currency" ]
 then
     total=$(echo "scale=10; $s_amount * $main_currency_price" | bc)
     [ $(echo $total | cut -c 1) = "." ] && total=0$total
-    sale_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"sell\", \"cur\": \"$main_currency\", \"amount\": $s_amount, \"ppu\": $main_currency_price, \"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\", \"comment\": \"$comment\" }"
+    sale_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"sell\", \"cur\": \"$main_currency\", \"amount\": $s_amount, \"ppu\": $main_currency_price, $vc_text\"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\", \"comment\": \"$comment\" }"
     buy_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"buy\", \"cur\": \"$currency_bought\", \"amount\": $b_amount, \"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\" }"
-fi
-
-if [ "$currency_bought" = "$main_currency" ]
+elif [ "$currency_bought" = "$main_currency" ]
 then
     total=$(echo "scale=10; $b_amount * $main_currency_price" | bc)
     [ $(echo $total | cut -c 1) = "." ] && total=0$total
     sale_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"sell\", \"cur\": \"$currency_sold\", \"amount\": $s_amount, \"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\", \"comment\": \"$comment\" }"
-    buy_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"buy\", \"cur\": \"$main_currency\", \"amount\": $b_amount, \"ppu\": $main_currency_price, \"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\" }"
+    buy_tr="{ \"timestamp\": \"$timestamp\", \"trtype\": \"buy\", \"cur\": \"$main_currency\", \"amount\": $b_amount, \"ppu\": $main_currency_price, $vc_text\"total\": $total, \"exchange\": \"$exchange\", \"ref\": \"$signature\" }"
 fi
 
 if [ "$currency_sold" != "$main_currency" ] && [ "$currency_bought" != "$main_currency" ]
 then
+    [ "$main_currency" = "ETH" ] && echo "Not implemented." && exit 1
     echo -n "Enter value of sale in $main_currency: "
     read main_currency_value
     echo -n "Enter amount of $main_currency fee (from $main_currency balance change): "
